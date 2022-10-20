c36ca1b65af8692a32c3f1e682fd36d09a0fd954
Jay <Nopenotj@gmail.com>
2022-10-20 15:20:04 +0800

    cleanup + added cors

--- src/app.js
+++ src/app.js
@@ -3,16 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes');
 var memesRouter = require('./routes/memes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

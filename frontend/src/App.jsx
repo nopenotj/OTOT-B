ed85f998826f67e4f17096ef90241cc635aafb4d
Jay <Nopenotj@gmail.com>
2022-10-26 20:19:46 +0800

    added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useEffect } from 'react'
+const HOST = 'localhost:8080'
+import { Center, Divider, Image, VStack } from '@chakra-ui/react'
+import { Badge } from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [memes, setMemes] = useState([])
+  useEffect(() => {
+    fetch(`http://${HOST}/memes/`).then(x => x.json()).then(setMemes)
+  },[])
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {memes.map((meme, i) => 
+      <Center>
+        <VStack>
+      <Image style={{height:"auto",width:500}}src={meme.link}/>
+      <p><Badge>{meme.votes} Upvotes</Badge> {meme.description}</p>
+
+      <Divider/>
+        </VStack>
+      </Center>
+      )}
+
     </div>
   )
 }
